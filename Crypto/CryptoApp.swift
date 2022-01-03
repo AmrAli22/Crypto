e5872ee356c316f4720355281ef648cde3f6496d
Amr Ali <amr.alifciscu@gmail.com>
2022-01-03 11:07:21 +0200

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Amr Ali on 03/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
